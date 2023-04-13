6de7eda3625235569dcf3e2d1d26ea59d1ae0811
Dominique Clark <melaydominique@gmail.com>
2023-04-13 14:23:20 -0400

    class added

--- cardify_back/cardify_backend/models.py
+++ cardify_back/cardify_backend/models.py
@@ -12,3 +12,9 @@ class Deck(models.Model):
     title = models.CharField(max_length=32)
     subject = models.CharField(choices=SUBJECT_CHOICES, default='math')
     comments = models.CharField()
+
+class Card(model.Models):
+    deck = models.ForeignKey(Deck)
+    question = models.CharField()
+    answer = models.CharField()
+    image = models.CharField()
